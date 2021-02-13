fe7850f791f855f38cbfab05ab0d07a67df8a9df
Rohit <48995037+rohit-vrrr@users.noreply.github.com>
2021-02-13 15:34:03 +0530

    Structured query with user input
    + server.js
    + index.html
    + package.json

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
+
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function(req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post('/', function(req, res) {
 
   /* openweathermap Endpoint */
-  const url = "https://api.openweathermap.org/data/2.5/weather?q=Bangalore&appid=2fae43311d006afcde91ba2ab480a9d7&units=metric";
+  const query = req.body.cityName;
+  const apiKey = "2fae43311d006afcde91ba2ab480a9d7";
+  const unit = "metric";
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + unit;
 
   /* Request thru Native https API */
   https.get(url, function(response) {
@@ -21,12 +31,12 @@ app.get('/', function(req, res) {
 
       res.write('<head><meta charset="utf-8"></head>');
       res.write("<h3>The weather is currently " + desc + ".</h3>");
-      res.write("<h1>The temperature in Bangalore is " + temp + " degrees Celcius.</h1>");
+      res.write("<h1>The temperature in " + query + " is " + temp + " degrees Celcius.</h1>");
       res.write("<img src=" + imageURL + ">");
       res.send();
     });
   });
-});
+})
 
 app.listen(3000, function() {
   console.log("Listening on port 3000");
